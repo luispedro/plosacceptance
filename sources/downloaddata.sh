432d8eb92eec79466df588b1ca7c8c7f0fb71142
Luis Pedro Coelho <luis@luispedro.org>
2013-05-28 18:19:57 +0200

    ENH Download some starting data

---
+++ sources/downloaddata.sh
@@ -0,0 +1,4 @@
+for spage in `seq 0 9`; do
+    wget 'http://www.plosone.org/search/advanced?pageSize=12&sort=Date%2C+newest+first&filterStartDate=2013-04-28T00%3A00%3A00Z&filterEndDate=2013-05-28T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage='$seq'&filterJournals=PLoSONE' --output-document="list${spage}.html"
+    sleep 1
+done
