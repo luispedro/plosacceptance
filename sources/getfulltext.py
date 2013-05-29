e488612eb59c32b42c05b88156fd6da603149bfa
Luis Pedro Coelho <luis@luispedro.org>
2013-05-29 16:19:41 +0200

    MIN A bit more data

--- sources/getfulltext.py
+++ sources/getfulltext.py
@@ -9,7 +9,7 @@ def get_uris(listf):
     dois = set([mat.group(1) for mat in pat.finditer(open(listf).read())])
     return [xml_uri(d) for d in dois]
 
-for i,f in enumerate(chain(*[get_uris('data/list{}.html'.format(i)) for i in xrange(10)])):
+for i,f in enumerate(chain(*[get_uris('data/list{}.html'.format(i)) for i in xrange(30)])):
     print "wget --output-document=data/output{}.xml '{}'".format(i,f)
     print "sleep 1"
 
