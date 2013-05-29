0b12ace00927b9c6b6e876001a8052049fe0a41d
Luis Pedro Coelho <luis@luispedro.org>
2013-05-29 16:41:54 +0200

    BUG Fix data download

--- sources/downloaddata.sh
+++ sources/downloaddata.sh
@@ -1,4 +1,6 @@
+mkdir -p data
 for spage in `seq 0 29`; do
-    wget 'http://www.plosone.org/search/advanced?pageSize=12&sort=Date%2C+newest+first&filterStartDate=2013-04-28T00%3A00%3A00Z&filterEndDate=2013-05-28T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage='$seq'&filterJournals=PLoSONE' --output-document="list${spage}.html"
+    wget 'http://www.plosone.org/search/advanced?pageSize=12&sort=Date%2C+newest+first&filterStartDate=2013-04-28T00%3A00%3A00Z&filterEndDate=2013-05-28T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage='$spage'&filterJournals=PLoSONE' --output-document="data/list${spage}.html"
     sleep 1
 done
+python getfulltext.py | sh -
