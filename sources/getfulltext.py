0dbf214c46a5d42d0409041449b1779a9164aba0
Luis Pedro Coelho <luis@luispedro.org>
2013-06-04 10:39:17 +0200

    RFCT Refactor code for multiple journals

--- sources/getfulltext.py
+++ sources/getfulltext.py
@@ -1,15 +1,25 @@
 from itertools import chain
 import re
 
-def xml_uri(info_doi):
-    return 'http://www.plosone.org/article/fetchObjectAttachment.action?uri={}&representation=XML'.format(info_doi)
+journaldata = {
+    'plos1' :  'plosone',
+    'ploscb' : 'ploscompbiol',
+    'plosgen': 'plosgenetics',
+    'plosbio': 'plosbiology',
+    'plosntd': 'plosntds',
+}
 
-def get_uris(listf):
-    pat = re.compile(r'href="http://www.plosone.org/article/(info.*);jsession')
+
+
+def get_uris(hostname, listf):
+    def xml_uri(info_doi):
+        return 'http://www.{}.org/article/fetchObjectAttachment.action?uri={}&representation=XML'.format(hostname,info_doi)
+    pat = re.compile(r'href="http://www.{}.org/article/(info.*);jsession'.format(hostname))
     dois = set([mat.group(1) for mat in pat.finditer(open(listf).read())])
     return [xml_uri(d) for d in dois]
 
-for i,f in enumerate(chain(*[get_uris('data/list{}.html'.format(i)) for i in xrange(30)])):
-    print "wget --output-document=data/output{}.xml '{}'".format(i,f)
-    print "sleep 1"
+for journal, hostname in journaldata.items():
+    for i,f in enumerate(chain(*[get_uris(hostname, 'data/{}/list{}.html'.format(journal, i)) for i in xrange(30)])):
+        print "wget --output-document=data/{}/output{}.xml '{}'".format(journal, i,f)
+        print "sleep 1"
 
