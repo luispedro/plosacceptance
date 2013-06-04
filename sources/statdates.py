f1a95562264337a938bf6f201135234505d2eb2a
Luis Pedro Coelho <luis@luispedro.org>
2013-06-04 17:41:23 +0200

    ENH Generalize for all journals

--- sources/statdates.py
+++ sources/statdates.py
@@ -4,10 +4,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 from lxml import etree
 from datetime import date
+from glob import glob
+from sys import argv
 
 def get_dates(ifile):
     et = etree.parse(ifile)
-    d0,d1 = et.findall('//date')
+    dates = et.findall('//date')
+    if len(dates) == 0:
+        return None
+    d0,d1 = dates
     received = None
     accepted = None
     for d0 in et.findall('//date'):
@@ -20,12 +25,19 @@ def get_dates(ifile):
              print d0.attrib
     return received, accepted
 
-deltas = []
-for i in xrange(360):
-    received, accepted = get_dates('data/output{}.xml'.format(i))
-    delta = accepted - received
-    deltas.append(delta.days)
+if len(argv) < 2:
+    print 'Argument needed'
 
+basedir = argv[1]
+journal = basedir[len('data/'):]
+
+deltas = []
+for f in glob(basedir+'/*.xml'):
+    dates = get_dates(f)
+    if dates is not None:
+        received, accepted = dates
+        delta = accepted - received
+        deltas.append(delta.days)
 deltas = np.array(deltas)
 
 style.use('ggplot')
@@ -35,11 +47,11 @@ c = gc(np.arange(800)) * len(deltas)
 plt.hist(deltas,np.arange(802), color='k')
 plt.plot(c, lw=8)
 plt.xlabel('Nr days')
-plt.ylabel("Papers (N=360)")
+plt.ylabel("Papers (N={})".format(len(deltas)))
 avg = np.mean(deltas)
 median = np.median(deltas)
 std = np.std(deltas)
 mode = c.argmax()
 plt.text(240,4,"Average is {} (std: {})\nMedian is {}\nMode (of KDE fit) is {}".format(int(avg),int(std),median,mode), fontdict={'size':24})
-plt.savefig('stataccept.png')
+plt.savefig('stataccept-{}.png'.format(journal))
 
