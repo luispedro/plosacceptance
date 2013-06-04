af2f7e385f7201af71473f400298ff84cca1cfb1
Luis Pedro Coelho <luis@luispedro.org>
2013-06-04 10:57:20 +0200

    ENH Get more files. Simpler local file encoding

--- sources/downloaddata.py
+++ sources/downloaddata.py
@@ -1,14 +1,14 @@
-from os import mkdir
+from os import mkdir, path
 
 
 journaldata = {
     #'plos1' : 'http://www.plosone.org/search/advanced?pageSize=12&sort=Date'
     #            '%2C+newest+first&filterStartDate=2013-04-28T00%3A00%3A00Z&'
     #            'filterEndDate=2013-05-28T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSONE',
-    'ploscb' : 'http://www.ploscompbiol.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2013-05-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSCompBiol',
-    'plosgen': 'http://www.plosgenetics.org/search/advanced;jsessionid=BADC342F94563D2D30B0643156770E46?sort=Date%2C+newest+first&filterStartDate=2013-05-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSGenetics',
-    'plosbio': 'http://www.plosbiology.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2013-05-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSBiology',
-    'plosntd': 'http://www.plosntds.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2013-05-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSNTD',
+    'ploscompbiol' : 'http://www.ploscompbiol.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2012-06-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSCompBiol',
+    'plosgenetics': 'http://www.plosgenetics.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2012-06-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSGenetics',
+    'plosbiology': 'http://www.plosbiology.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2012-06-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSBiology',
+    'plosntds': 'http://www.plosntds.org/search/advanced?sort=Date%2C+newest+first&filterStartDate=2012-06-04T00%3A00%3A00Z&filterEndDate=2013-06-04T23%3A59%3A59Z&unformattedQuery=*%3A*&startPage={}&filterJournals=PLoSNTD',
 }
 
 
@@ -25,8 +25,10 @@ def retrieve_for(journal, baseurl):
 
     for i in range(30):
         fullurl = baseurl.format(i)
-        print( 'wget "{}" --output-document="{}/list{}.html"'.format(fullurl, outputdir, i))
-        print('sleep 1')
+        outfile = "{}/list{}.html".format(outputdir, i)
+        if not path.exists(outfile):
+            print( 'wget "{}" --output-document="{}"'.format(fullurl, outfile))
+            print('sleep 1')
 
 if __name__ == '__main__':
     for j,base in journaldata.items():
--- sources/getfulltext.py
+++ sources/getfulltext.py
@@ -1,25 +1,28 @@
+from os import path
 from itertools import chain
 import re
 
-journaldata = {
-    'plos1' :  'plosone',
-    'ploscb' : 'ploscompbiol',
-    'plosgen': 'plosgenetics',
-    'plosbio': 'plosbiology',
-    'plosntd': 'plosntds',
-}
+journals = [
+#   'plos1' :  'plosone',
+    'ploscompbiol',
+    'plosgenetics',
+    'plosbiology',
+    'plosntds',
+]
 
 
 
-def get_uris(hostname, listf):
+def get_uris(journal, listf):
     def xml_uri(info_doi):
-        return 'http://www.{}.org/article/fetchObjectAttachment.action?uri={}&representation=XML'.format(hostname,info_doi)
-    pat = re.compile(r'href="http://www.{}.org/article/(info.*);jsession'.format(hostname))
+        return 'http://www.{}.org/article/fetchObjectAttachment.action?uri={}&representation=XML'.format(journal,info_doi)
+    pat = re.compile(r'href="http://www.{}.org/article/(info.*);jsession'.format(journal))
     dois = set([mat.group(1) for mat in pat.finditer(open(listf).read())])
     return [xml_uri(d) for d in dois]
 
-for journal, hostname in journaldata.items():
-    for i,f in enumerate(chain(*[get_uris(hostname, 'data/{}/list{}.html'.format(journal, i)) for i in xrange(30)])):
-        print "wget --output-document=data/{}/output{}.xml '{}'".format(journal, i,f)
-        print "sleep 1"
+for journal in journals:
+    for i,f in enumerate(chain(*[get_uris(journal, 'data/{}/list{}.html'.format(journal, i)) for i in xrange(30)])):
+        outfile = "data/{}/output{}.xml".format(journal, i)
+        if not path.exists(outfile):
+            print "wget --output-document={} '{}'".format(outfile,f)
+            print "sleep 1"
 
