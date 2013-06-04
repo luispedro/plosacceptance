67cd0c65ca2c5be0d3c21d47ff7d58c64f72a10e
Luis Pedro Coelho <luis@luispedro.org>
2013-06-04 14:54:09 +0200

    ENH Save in DOI-named file. Checck if exists

--- sources/getfulltext.py
+++ sources/getfulltext.py
@@ -1,3 +1,4 @@
+from random import randint
 from os import path
 from itertools import chain
 import re
@@ -17,12 +18,12 @@ def get_uris(journal, listf):
         return 'http://www.{}.org/article/fetchObjectAttachment.action?uri={}&representation=XML'.format(journal,info_doi)
     pat = re.compile(r'href="http://www.{}.org/article/(info.*);jsession'.format(journal))
     dois = set([mat.group(1) for mat in pat.finditer(open(listf).read())])
-    return [xml_uri(d) for d in dois]
+    return [(xml_uri(d),d) for d in dois]
 
 for journal in journals:
-    for i,f in enumerate(chain(*[get_uris(journal, 'data/{}/list{}.html'.format(journal, i)) for i in xrange(30)])):
-        outfile = "data/{}/output{}.xml".format(journal, i)
+    for f,d in chain(*[get_uris(journal, 'data/{}/list{}.html'.format(journal, i)) for i in xrange(30)]):
+        outfile = "data/{}/{}.xml".format(journal, d)
         if not path.exists(outfile):
             print "wget --output-document={} '{}'".format(outfile,f)
-            print "sleep 1"
+            print "sleep {}".format(randint(1,12))
 
